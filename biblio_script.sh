f2d531dddfaa001320d3ff6652bb0088ba631e96
Luong Minh Thang <luong.m.thang@gmail.com>
2010-09-18 04:02:02 +0800

    Allow input file of (MODS) file + add script relative paths

--- biblio_script.sh
+++ biblio_script.sh
@@ -5,12 +5,12 @@ import os,sys,getopt,re
 
 # paths to executables 
 # Thang v100901: minor modifications in the code so that it doesn't matter if the below directory paths end with / or not
-PARSCIT_PATH="/Applications/ParsCit/bin/"
-BIBUTILS_PATH="/Applications/bibutils_4.8/"
-SAXON_PATH="/Applications/saxonhe9-2-1-2j/saxon9he.jar"
+PARSCIT_PATH=sys.path[0] + "/../../bin/"
+BIBUTILS_PATH=sys.path[0] + "/bibutils_4.10"
+SAXON_PATH=sys.path[0] + "/saxonhe9-2-1-2j/saxon9he.jar"
 
 # paths to resources
-XSLT_TRANFORM_PATH="./parscit2mods.xsl"
+XSLT_TRANFORM_PATH=sys.path[0] + "/parscit2mods.xsl"
 
 def parscit_to_mods(parscit_out):
 	saxon_cmd="java -jar %s -xsl:%s -s:%s" %(SAXON_PATH,XSLT_TRANFORM_PATH,parscit_out)
@@ -24,32 +24,32 @@ def export_mods(mods_xml, out_type):
 	return out
 
 def usage():
-	print "Usage: %s [-h] [-m <mode>] [-o <outputType>] <inputFile> <outDir>" %(sys.argv[0])
+	print "Usage: %s [-h] [-i <inputType>] [-o <outputType>] <inputFile> <outDir>" %(sys.argv[0])
         print "Options:"
         print "\t-h\tPrint this mesage"
-        print "\t-m <mode>\tMode=\"all\" (full-text input) or \"ref\" (input contains only individual reference strings, one per line) (default=\"ref\")"
+        print "\t-i <inputType>\tType=\"all\" (full-text input),\"ref\" (input contains only individual reference strings, one per line), or \"mods\" (ParsCit extraction step will be skipped to reuse MODS file) (default=\"ref\")"
         print "\t-o <outputType>\tType=(ads|bib|end|isi|ris|wordbib) (default=bib)"
 
 # Thang v100901: process argv array using getopt
 def process_argv(argv):
   try:
-    opts, args = getopt.getopt(argv[1:], "hm:o:", ["help", "mode=", "output="])
+    opts, args = getopt.getopt(argv[1:], "hi:o:", ["help", "input=", "output="])
   except getopt.GetoptError, err:
     print str(err)
     usage()
     sys.exit(2)
 
-  mode = "ref"
+  in_type = "ref"
   out_type = "bib"
 
   for o, a in opts:
     if o in ("-h", "--help"):
       usage()
       sys.exit()
-    elif o in ("-m", "--mode"):
-      mode = a
-      if(mode != "all" and option != "ref"):
-        sys.stderr.write("#! mode \"%s\" is neither \"ref\" nor \"all\"\n" % option)
+    elif o in ("-i", "--input"):
+      in_type = a
+      if(not re.match("(all|ref|mods)", in_type)):
+        sys.stderr.write("#! in_type \"%s\" does not match (all|ref|mods)\n" % in_type)
         sys.exit(1)
     elif o in ("-o", "--output"):
       out_type = a
@@ -59,7 +59,7 @@ def process_argv(argv):
         sys.exit(1)
   
     else:
-      assert False, "unhandled mode"
+      assert False, "unhandled in_type"
 
   # get inp_file, out_dir & check validity
   inp_file = ""
@@ -70,8 +70,8 @@ def process_argv(argv):
   else:
     usage()
     sys.exit(1)
- 
-  sys.stderr.write("# (mode, outputType, inputFile, outDir) = (\"%s\", \"%s\", \"%s\", \"%s\")\n" %(mode, out_type, inp_file, out_dir))
+
+  sys.stderr.write("# (in_type, outputType, inputFile, outDir) = (\"%s\", \"%s\", \"%s\", \"%s\")\n" %(in_type, out_type, inp_file, out_dir))
 
   # check if the input file exists
   if not os.path.isfile(inp_file):
@@ -83,33 +83,38 @@ def process_argv(argv):
     sys.stderr.write("#! Directory \"%s\" doesn't exist. Creating ...\n" % out_dir)
     os.makedirs(out_dir)
 
-  return (out_type, mode, inp_file, out_dir)
+  return (out_type, in_type, inp_file, out_dir)
 # End Thang v100901: process argv array
 
-### Main program ###        
-(out_type, mode, inp_file, out_dir) = process_argv(sys.argv)
-
+############
+### MAIN ###
+############
+(out_type, in_type, inp_file, out_dir) = process_argv(sys.argv)
 print "# Extracting references from the input file..."
 
-# Thang v100901: handle mode
-if (mode == "ref"):
+# Thang v100901: handle in_type
+if (in_type == "ref"):
   parscit_out = os.popen("%s/parseRefStrings.pl %s" %(PARSCIT_PATH,inp_file)).readlines()
-else:
+elif(in_type == "all"):
   parscit_out = os.popen("%s/citeExtract.pl -m extract_citations %s" %(PARSCIT_PATH,inp_file)).readlines()
   
-parscit_xml='%s/parscit_temp.xml'%out_dir
-file = open(parscit_xml,'w')
-for line in parscit_out:
+if(in_type != "mods"):
+  parscit_xml='%s/parscit_temp.xml'%out_dir
+  file = open(parscit_xml,'w')
+  for line in parscit_out:
 	file.write(line)
-file.close()
+  file.close()
 	
 # transform parscit's output into mods 3.x
-parscit_mods='%s/parscit_mods.xml'%out_dir
-file = open(parscit_mods,'w')
-for line in parscit_to_mods(parscit_xml):
+if(in_type != "mods"):
+  parscit_mods='%s/parscit_mods.xml'%out_dir
+  file = open(parscit_mods,'w')
+  for line in parscit_to_mods(parscit_xml):
 	file.write(line)
-file.close()
-	
+  file.close()
+else: # already an MODS file, copy over
+  parscit_mods = inp_file
+
 # transform mods intermediate xml into other export format
 # Thang v100901: modify to handle multiple format 
 export_file='%s/parscit.%s' %(out_dir, out_type)
